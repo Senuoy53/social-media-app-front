d7222fb04439047bcbc302bae9feb6a26144d07f
Senuoy53 <lamrrani.younes@gmail.com>
2022-05-06 23:29:20 +0000

    [NDIN-45] navbar v1

---
+++ src/components/Navbar/NavbarWrapper.ts
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+import { mobile, mobile600, mobile350 } from "../../styles/responsive";
+
+const NavbarWrapper = styled.div`
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 3%;
+    ${mobile({
+      padding: "0px 1%",
+    })}
+  }
+
+  .navMenu {
+    display: flex;
+
+    a {
+      color: ${({ theme }) => theme.colors.White};
+    }
+    .active::after {
+      content: "";
+      width: 100%;
+      height: 4px;
+      position: absolute;
+      bottom: -10px;
+      left: 0;
+      background-color: ${({ theme }) => theme.colors.White};
+    }
+
+    .navBox {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      text-decoration: none;
+      margin-left: 40px;
+      position: relative;
+
+      ${mobile350({
+        marginLeft: "10px",
+      })}
+
+      &:hover::after {
+        content: "";
+        width: 100%;
+        height: 4px;
+        position: absolute;
+        bottom: -10px;
+        left: 0;
+        background-color: ${({ theme }) => theme.colors.White};
+      }
+
+      .navIcon {
+        font-size: 35px;
+        ${mobile600({
+          fontSize: "25px",
+        })}
+      }
+
+      .navText {
+        margin-top: -3px;
+        font-size: 10px;
+        display: block;
+
+        ${mobile({
+          display: "none",
+        })};
+      }
+    }
+  }
+
+  .logo-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 150px;
+    ${mobile({
+      width: "100px",
+    })}
+
+    .logo {
+      width: 80%;
+    }
+
+    .logoTitle {
+      font-size: 12px;
+
+      ${mobile({
+        fontSize: "8px",
+      })}
+    }
+  }
+`;
+
+export default NavbarWrapper;
