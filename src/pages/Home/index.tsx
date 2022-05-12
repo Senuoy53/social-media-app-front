933c453c855dfd103f6636d07a26eaa60091592f
Hermes-fire <amine.sadali@gmail.com>
2022-05-12 12:03:08 +0100

    merge-conflict-solving

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,43 +1,19 @@
-import { Link } from "react-router-dom";
-import { axiosApi } from "../../utils/request";
-import { BACK_URL } from "../../variables";
-import Post from '../../components/Post'
-
-const signout = () => {
-  if (typeof window.localStorage !== "undefined") {
-    let jwt = localStorage.getItem("jwt");
-    //  Take accessToken
-    let accessToken = JSON.parse(jwt!).accessToken;
-    //  Take refreshToken
-    let refreshToken = JSON.parse(jwt!).refreshToken;
-
-    axiosApi({
-      method: "POST",
-      url: `${BACK_URL}/signout`,
-      headers: {
-        Accept: "application/json",
-        authorization: "Bearer " + accessToken,
-      },
-      data: { refreshToken: refreshToken },
-    })
-      .then((res) => {
-        console.log("singout res", res);
-        localStorage.removeItem("jwt");
-      })
-      .catch((err) => {
-        console.log("singout err", err.response.data.error);
-      });
-  }
-};
+import Navbar from "../../components/Navbar";
+import FeedContainer from "../../containers/FeedContainer";
+import LeftBar from "../../containers/LeftBar";
+import RightBar from "../../containers/RightBar";
+import HomeWrapper from "./HomeWrapper";
 
 const Home = () => {
   return (
-    <div>
-      <Post/>
-      <Link to="/signin" onClick={signout}>
-        <button>LOGOUT</button>
-      </Link>
-    </div>
+    <HomeWrapper>
+      <Navbar />
+      <div className="main">
+        <RightBar />
+        <FeedContainer />
+        <LeftBar />
+      </div>
+    </HomeWrapper>
   );
 };
 
