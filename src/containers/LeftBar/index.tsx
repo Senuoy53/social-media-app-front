5a63a2e5231e456e69292b8037abde9ff0d95494
Senuoy53 <lamrrani.younes@gmail.com>
2022-06-15 22:27:20 +0000

    [NDIN-84] add  user profile container v1

--- src/containers/LeftBar/index.tsx
+++ src/containers/LeftBar/index.tsx
@@ -1,5 +1,5 @@
 import LeftBarWrapper from "./LeftBarWrapper";
-import { Room } from "@mui/icons-material";
+import { Cake, Female, Male, Work } from "@mui/icons-material";
 import { createStructuredSelector } from "reselect";
 import { makeSelectEvBtnClicked } from "../../components/Navbar/selectors";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,12 +8,25 @@ import EventComponent from "../../components/EventComponent";
 import { EventTitle } from "./constants";
 import { useCallback, useEffect, useState } from "react";
 import { setEvBtnClicked } from "../../components/Navbar/actions";
+import ButtonCustom from "../../components/ButtonCustom";
+import Moment from "react-moment";
+import DefaultProfilePicture from "../../assets/img/profil.jpg";
 
 const navBarState = createStructuredSelector({
   evBtnClicked: makeSelectEvBtnClicked(),
 });
 
 const LeftBar = () => {
+  let jwt: string | null = "";
+  let user: any = "";
+  // Get user from localstorage
+  if (typeof window.localStorage !== "undefined") {
+    jwt = localStorage.getItem("jwt");
+    user = JSON.parse(jwt!).user;
+  }
+
+  console.log("user.dateOfBirth", user.dateOfBirth);
+
   // Selectors
   const { evBtnClicked } = useSelector(navBarState);
 
@@ -42,9 +55,75 @@ const LeftBar = () => {
     };
   }, [handleNavigation]);
 
+  // SubmitPost
+  const handleClick = () => {};
+
   return (
     <LeftBarWrapper evBtnClicked={evBtnClicked}>
-      {/* <div className="rightBarMenu"> */}
+      {/* userProfileContainer */}
+      <div className="userProfileContainer">
+        <div className="topContainer">
+          {user.profilePicture ? (
+            <img src={user.profilePicture} className="profileImg" />
+          ) : (
+            <img src={DefaultProfilePicture} className="profileImg" />
+          )}
+
+          <div className="userName">{user.fname + " " + user.lname}</div>
+          <ButtonCustom className="btn-2" onClick={handleClick}>
+            {/* {ButtonField.POST} */}
+            Edit Profile
+          </ButtonCustom>
+        </div>
+        <div className="bottomContainer">
+          <h4 className="profileTitle">About</h4>
+          <div className="profileInfos">
+            <div className="profileInfosBox">
+              <Work className="profileIcon" />
+              {user.jobTitle ? (
+                <div className="profileText">{user.jobTitle}</div>
+              ) : (
+                <div className="profileText">Job title :</div>
+              )}
+            </div>
+            <div className="profileInfosBox">
+              {user.gender ? (
+                user.gender &&
+                (user.gender == "Male" ? (
+                  <>
+                    <Male className="profileIcon" />
+                    <div className="profileText">Male</div>
+                  </>
+                ) : (
+                  <>
+                    <Female className="profileIcon" />
+                    <div className="profileText">Female</div>
+                  </>
+                ))
+              ) : (
+                <>
+                  <Male className="profileIcon" />
+                  <div className="profileText">Gender :</div>
+                </>
+              )}
+            </div>
+
+            <div className="profileInfosBox">
+              <Cake className="profileIcon" />
+              {user.dateOfBirth ? (
+                <div className="profileText">
+                  Born on{" "}
+                  <Moment format="MMM, DD, YYYY">{user.dateOfBirth}</Moment>
+                </div>
+              ) : (
+                <div className="profileText">Date of birth : </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/*eventsContainer  */}
       <div className="eventsContainer">
         <h4 className="evTitle">{EventTitle.evTitle}</h4>
         <div className="evList">
